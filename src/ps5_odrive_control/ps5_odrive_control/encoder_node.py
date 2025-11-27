57271213864ce4ecaf157c2284de99256757991c
Trevor Perey <trevorperey@gmail.com>
2025-11-27 13:54:19 -0800

    Successful bytewise Encoder

--- src/ps5_odrive_control/ps5_odrive_control/encoder_node.py
+++ src/ps5_odrive_control/ps5_odrive_control/encoder_node.py
@@ -5,6 +5,8 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import serial
 
+RAD_PER_PULSE = (2.0 * 3.1415926535)/2400
+
 class SimpleLowFilter():
     def __init__(self, dt, cutoff):
         self._dt = dt
@@ -43,6 +45,8 @@ class EncoderNode(Node):
             self.get_logger().error(f"Failed to connect to Arduino: {e}")
             raise e
 
+        self._buf = bytearray()  # Serial buffer
+
         # Create a fast timer to poll serial port
         self._dt = 0.001
         self.timer = self.create_timer(self._dt, self.timer_callback)  # 100 Hz polling, much slower than Arduino, just takes newest message
@@ -52,28 +56,57 @@ class EncoderNode(Node):
 
     def timer_callback(self):
         try:
-            # Read all available lines
-            while self.ser.in_waiting:
-                try:
-                    line = self.ser.readline().decode('utf-8').strip()
-                    if line:
-                        try:
-                            val = float(line)
 
-                            # Update velo
-                            velo = self._vel_filter.update(val)
+            # Read bytes
+            if self.ser.in_waiting:  # Bring in available byte
+                self._buf += self.ser.read(self.ser.in_waiting)
+            
+            # Process new msg
+            while len(self._buf) >= 7:  # If at least 7 bytes, then new msg has arrived
 
-                            # Msg
-                            msg = Float32MultiArray()
-                            msg.data = [val, velo]  # Zero velocity for now
-                            self.pub.publish(msg)
-                            #self.get_logger().info(f"_ deg: {val}")
-                        except ValueError:
-                            # Ignore malformed lines
-                            self.get_logger().info("~~~Mishapen msg")
-                            continue
-                except UnicodeDecodeError:
-                    self.get_logger().info("!!!Bad decode")
+                # Find start
+                start_idx = self._buf.find(b'\xAA\x55')
+
+                if start_idx == -1:
+                    # No starts found, so clear
+                    self._buf.clear()
+                    self.get_logger().warn("---Missing start byte")
+
+                    break
+                elif start_idx > 0:
+                    # Discard before start
+                    self._buf = self._buf[start_idx:]
+                    self.get_logger().warn("```Clearing before start byte")
+                
+                # Extract
+                payload = self._buf[2:6]
+                checksum = self._buf[6]
+
+                # Compute checksum
+                cs_calc = (0xAA + 0x55 + sum(payload)) & 0xFF
+                if cs_calc != checksum:
+                    self.get_logger().warn("+++Checksum mismatch, discarding packet")
+                    self._buf = self._buf[1:]  # discard first byte and retry
+                    continue
+
+                # Convert payload to signed 32-bit integer (little-endian)
+                val = int.from_bytes(payload, byteorder='little', signed=True)
+                val_rad = float(val) * RAD_PER_PULSE  # [rad] conversion
+                # self.get_logger().info(f"Raw bytes: {list(self._buf[0:6])}")
+                # self.get_logger().info(f"val_rad = {val_rad}")
+
+                # Update velocity
+                velo = self._vel_filter.update(val_rad)
+
+                # Msg
+                msg = Float32MultiArray()
+                msg.data = [val_rad, velo]  # Zero velocity for now
+                self.pub.publish(msg)
+                #self.get_logger().info(f"_ deg: {val}")
+
+                # Remove processed packet
+                self._buf = self._buf[7:]
+
         except serial.SerialException as e:
             self.get_logger().error(f"Serial error: {e}")
 
--- src/ps5_odrive_control/ps5_odrive_control/furata_controller.py
+++ src/ps5_odrive_control/ps5_odrive_control/furata_controller.py
@@ -14,7 +14,7 @@ from odrive.utils import request_state
 import numpy as np
 
 REV_TO_RAD = 2*np.pi  # Converts rev to radians
-SMALL_ANGLE = 0.2  # Radians about t2 = pi where controller actually kicks in
+SMALL_ANGLE = 0.6  # Radians about t2 = pi where controller actually kicks in
 
 class OControlState(Enum):
     # For preventing crazy motions when pendulum in weird states
@@ -42,27 +42,34 @@ class FurataController(Node):
         self._odrv = get_Odrive_init(Vmax = V_MAX, ConType = ControlMode.TORQUE_CONTROL,
                                      shouldCalibrate = self._doCal, shouldErase = self._doErase, shouldReconfig = self._doRecon)
         self.get_logger().info("Odrive prepped :)")
+        self._odrv.clear_errors()  # Clear errors on start
         self._odrv.axis0.pos_estimate = 0.0  # 0 the position on start
-        self._odrv.axis0.controller.config.input_mode = InputMode.PASSTHROUGH
+        self._odrv.axis0.controller.config.input_mode = InputMode.PASSTHROUGH #InputMode.VEL_RAMP
         
         self._control_state = OControlState.OFF  # Start out off
 
         # Subscribers
         self.encoder_sub = self.create_subscription(Float32MultiArray, 'encoder_furata', self.encoder_callback, 10)
     
+        # Controller
+        #self._lqr_K = np.array([-0.31622777, 9.16100057, -0.8506804, 0.84424739])  # Copied from simulator, with mega Link 1 damping
+        self._lqr_K = np.array([-0.31622777, 7.21298925, -0.40204377, 0.69358956])
+        self._q_equ = np.array([0.0, np.pi, 0.0, 0.0])
+
     def encoder_callback(self, msg):
-        self.get_logger().info("Callback tripped")
 
         """ State machine for LQR encoder """
         t1 = (self._odrv.axis0.pos_estimate)*REV_TO_RAD
         t1d = (self._odrv.axis0.vel_estimate)*REV_TO_RAD
         t2 = msg.data[0]
         t2d = msg.data[1]  # Parse from encoder
+        
+        q = np.array([t1, t2, t1d, t2d])
 
         if self._control_state == OControlState.OFF:
 
             # Hit linear region, so re-engage
-            if (t2 < (np.pi + SMALL_ANGLE)) or (t2 > (np.pi - SMALL_ANGLE)):
+            if (t2 < (np.pi + SMALL_ANGLE)) and (t2 > (np.pi - SMALL_ANGLE)):
                 request_state(self._odrv.axis0, AxisState.CLOSED_LOOP_CONTROL)  # Enable O-drive
                 self._control_state = OControlState.ON
                 self.get_logger().info("---Enabling motor")
@@ -74,11 +81,15 @@ class FurataController(Node):
         elif self._control_state == OControlState.ON:
 
             # Still in linear region, so run LQR controller
-            if (t2 < (np.pi + SMALL_ANGLE)) or (t2 > (np.pi - SMALL_ANGLE)):
-                pass # For initial testing
+            if (t2 < (np.pi + SMALL_ANGLE)) and (t2 > (np.pi - SMALL_ANGLE)):
+                tau = ((-self._lqr_K) @ (q - self._q_equ))
+                tau = -tau*0.5 # For some reason, WAYYYY too large
+                # Also, I think sign at motor level is opposite
+                self.get_logger().info(f"Commanding torque = {tau}")
+                self._odrv.axis0.controller.input_torque = tau
             else:
                 # Stay in IDLE
-                self._odrv.axis0.controller.input_torque = 0.0
+                self._odrv.axis0.controller.input_vel = 0.0
                 request_state(self._odrv.axis0, AxisState.IDLE)
                 self._control_state = OControlState.OFF
                 self.get_logger().info("~~~DISabling motor")
