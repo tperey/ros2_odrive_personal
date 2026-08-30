19f2c421ce40cfb1c127e89d943bb26e8e74a981
Trevor Perey <trevorperey@gmail.com>
2026-08-30 13:53:38 -0700

    Good pend sine sid shit

--- Teensy_ODrive_PIO/src/sine_tau_pend_sid/sine_tau_pend_sid.cpp
+++ Teensy_ODrive_PIO/src/sine_tau_pend_sid/sine_tau_pend_sid.cpp
@@ -37,23 +37,28 @@ AccelKF thisKF = AccelKF(xi[0], xi[1], xi[2], R, Q, dt, Pi);
 
 /* ODRIVE SETUP */
 #define SINE_TORQUE_BASE_AMP 0.11
-constexpr uint8_t n_runs = 11;
+constexpr uint8_t n_runs = 12;
 OdriveSinusoidTorqueSID st_sid_runs[n_runs] = {
     // { {SINE_TORQUE_BASE_AMP}, {0.05}, 4, 1}, // Get static friciton data
     // { {0.4}, {2.0}, 100, 1}, // Firmly achieves full pendulum rotation
     // { {SINE_TORQUE_BASE_AMP}, {0.1}, 4, 1}, // Single frequency examples
     // { {SINE_TORQUE_BASE_AMP}, {0.2}, 4, 1},
-    { {1.5*SINE_TORQUE_BASE_AMP, 1.5*SINE_TORQUE_BASE_AMP/2}, {1.0, 2.0}, 10, 2}, // Good at flipping
-    { {SINE_TORQUE_BASE_AMP}, {0.5}, 10, 1},
-    { {SINE_TORQUE_BASE_AMP}, {1.0}, 20, 1},
-    { {SINE_TORQUE_BASE_AMP}, {2.0}, 20, 1},
-    { {4*SINE_TORQUE_BASE_AMP}, {5.0}, 20, 1}, // High accel, low speed (UNTESTED)
-    { {SINE_TORQUE_BASE_AMP}, {10.0}, 20, 1},
-    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {2.0, 5.0}, 10, 2},
-    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {0.5, 2.0}, 10, 2},
-    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {1.0, 2.0}, 10, 2}, // Good at flipping
-    { {0.15, 0.05}, {0.5, 2.5}, 10, 2},
-    { {0.2, 0.075}, {1.0, 2.0}, 10, 2}
+    { {3.0*SINE_TORQUE_BASE_AMP}, {2.0}, 20, 2},
+    { {2.0*SINE_TORQUE_BASE_AMP}, {2.0}, 20, 2},
+    { {1.5*SINE_TORQUE_BASE_AMP, 1.5*SINE_TORQUE_BASE_AMP/2}, {1.0, 2.0}, 20, 2}, // Good at flipping
+    // { {SINE_TORQUE_BASE_AMP}, {0.5}, 10, 1},
+    // { {SINE_TORQUE_BASE_AMP}, {1.0}, 20, 1},
+    // { {SINE_TORQUE_BASE_AMP}, {2.0}, 20, 1},
+    { {4*SINE_TORQUE_BASE_AMP}, {5.0}, 40, 1}, // High accel, low speed
+    // { {SINE_TORQUE_BASE_AMP}, {10.0}, 20, 1},
+    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {2.0, 5.0}, 20, 2},
+    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {0.5, 2.0}, 20, 2},
+    { {SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP/2}, {1.0, 2.0}, 20, 2}, // Good at flipping
+    { {2*SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP}, {2.0, 5.0}, 20, 2},
+    { {2*SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP}, {0.5, 2.0}, 20, 2},
+    { {2*SINE_TORQUE_BASE_AMP, SINE_TORQUE_BASE_AMP}, {1.0, 2.0}, 20, 2}, // Good at flipping
+    { {0.15, 0.05}, {0.5, 2.5}, 20, 2},
+    { {0.2, 0.075}, {1.0, 2.0}, 20, 2}
 };
 volatile static bool status_ok = true;
 
@@ -69,10 +74,10 @@ void setup() {
 
 void loop() {
     for (uint8_t i = 0; i < n_runs; i++) {
-        status_ok = perform_sid_with_pend_sinetorque(st_sid_runs[i], &thisKF, &furataEncoder, RAD_PER_PULSE);
-
         while (!status_ok) {
             // Infinite loop if stop msg received
         }
+        status_ok = perform_sid_with_pend_sinetorque(st_sid_runs[i], &thisKF, &furataEncoder, RAD_PER_PULSE);
     }
+    status_ok = false;
 }
